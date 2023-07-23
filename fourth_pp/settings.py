2735059edcf281c77778607ea4dfbc71c73c9a78
zemaciel <joemacbr@gmail.com>
2023-07-23 17:12:34 +0000

    Prepare settings for deployment

--- fourth_pp/settings.py
+++ fourth_pp/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-xtcs#irev&&7g95o-_4cv+kz+m!ps$!&$a#p7dgqfpvk5^8e(!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPIONS = 'SAMEORIGIN'
 
